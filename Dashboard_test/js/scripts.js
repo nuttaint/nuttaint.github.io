85510b2d66192ca8349a9c91e5f0422c6d2fd585
NUTTA <56569691+nuttaint@users.noreply.github.com>
2023-07-13 15:27:48 +0700

    nnew

---
+++ Dashboard_test/js/scripts.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+
+  // // Initialize select2
+  $("#selUser").select2({
+
+  });
+
+  // Read selected option
+  $('#but_read').click(function () {
+    var username = $('#selUser option:selected').text();
+    var userid = $('#selUser').val();
+
+    location.replace(username + ".html");
+
+  });
+});
