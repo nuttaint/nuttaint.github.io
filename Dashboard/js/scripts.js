6d2eae54c1bc7502ef053498cee11273c5ae46fc
NUTTA <56569691+nuttaint@users.noreply.github.com>
2023-07-13 18:09:07 +0700

    update

--- Dashboard/js/scripts.js
+++ Dashboard/js/scripts.js
@@ -14,3 +14,21 @@ $(document).ready(function () {
 
   });
 });
+
+$(document).ready(function () {
+
+  // // Initialize select2
+  $("#selLine").select2({
+
+  });
+
+  // Read selected option
+  $('#but_line').click(function () {
+    var username = $('#selLine option:selected').text();
+    var userid = $('#selLine').val();
+
+    location.replace(username + ".html");
+
+  });
+});
+
